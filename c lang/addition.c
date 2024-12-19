691a4839b07521adef28ae652c58f6babd5bad58
Harshani Patil <patilharshani2483@gmail.com>
2024-12-19 22:36:59 +0530

    test

---
+++ c lang/addition.c
@@ -0,0 +1,8 @@
+#include<stdio.h>>
+main()
+{   int a=60, b=20, c;
+	printf("hello world");
+	printf("welcome to my website");
+	c=a+b;
+	printf("perform the addition operation" a=%d,b=%d,c=%d,a,b,c);
+}
